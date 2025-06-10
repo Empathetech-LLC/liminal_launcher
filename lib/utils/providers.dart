de481ca86d1bee0b4d269153a6a894d50396b7c3
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-09 22:45:07 -0700

    using in house app info

--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -1,13 +1,14 @@
-/* smoke_signal
+/* empathetech_launcher
  * Copyright (c) 2025 Empathetech LLC. All rights reserved.
  * See LICENSE for distribution and usage details.
  */
 
+import './export.dart';
+
 import 'package:flutter/material.dart';
-import 'package:installed_apps/index.dart';
 
 class AppInfoProvider extends ChangeNotifier {
-  List<AppInfo> _apps;
+  final List<AppInfo> _apps;
 
   AppInfoProvider(List<AppInfo> apps) : _apps = apps;
 
