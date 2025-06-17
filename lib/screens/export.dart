45f8aab704cad6300b81b7f78421aa840c71c30d
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-16 21:01:49 -0700

    alphabet

--- lib/screens/export.dart
+++ lib/screens/export.dart
@@ -9,32 +9,36 @@ export 'error.dart';
 export 'app_list.dart';
 export 'home.dart';
 
+export 'settings/color_settings.dart';
+export 'settings/design_settings.dart';
+export 'settings/ez_settings_home.dart';
+export 'settings/image_settings.dart';
+export 'settings/layout_settings.dart';
 export 'settings/settings_home.dart';
 export 'settings/text_settings.dart';
-export 'settings/layout_settings.dart';
-export 'settings/color_settings.dart';
-export 'settings/image_settings.dart';
-export 'settings/design_settings.dart';
 
 // Route names //
 
 /// 'app-list'
 const String appListPath = 'app-list';
 
+/// 'color-settings'
+const String colorSettingsPath = 'color-settings';
+
+/// 'design-settings'
+const String designSettingsPath = 'design-settings';
+
+/// 'ez-settings-home'
+const String ezSettingsHomePath = 'ez-settings-home';
+
+/// 'image-settings'
+const String imageSettingsPath = 'image-settings';
+
+/// 'layout-settings'
+const String layoutSettingsPath = 'layout-settings';
+
 /// 'settings-home'
 const String settingsHomePath = 'settings-home';
 
 /// 'text-settings'
 const String textSettingsPath = 'text-settings';
-
-/// 'layout-settings'
-const String layoutSettingsPath = 'layout-settings';
-
-/// 'color-settings'
-const String colorSettingsPath = 'color-settings';
-
-/// 'image-settings'
-const String imageSettingsPath = 'image-settings';
-
-/// 'design-settings'
-const String designSettingsPath = 'design-settings';
