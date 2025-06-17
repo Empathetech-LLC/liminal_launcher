0284e82a28579aa3ecac2404a371fc1134ed5468
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-17 13:48:56 -0700

    custom equals

--- lib/utils/models.dart
+++ lib/utils/models.dart
@@ -10,11 +10,21 @@ class AppInfo {
   final String package;
   final Uint8List? icon;
 
-  AppInfo({required this.label, required this.package, this.icon});
+  const AppInfo({required this.label, required this.package, this.icon});
 
   factory AppInfo.fromMap(Map<String, dynamic> map) => AppInfo(
         label: map['label'],
         package: map['package'],
         icon: map['icon'],
       );
+
+  @override
+  bool operator ==(Object other) =>
+      other is AppInfo && package == other.package;
+
+  @override
+  int get hashCode => package.hashCode;
+
+  @override
+  String toString() => 'app_label: $label, app_package: $package';
 }
