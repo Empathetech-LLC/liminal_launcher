f5c8950e95aa4e4e70f964b8ac21fcb8e2fb3766
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-18 10:45:29 -0700

    fixed bug

--- lib/screens/settings/settings_home.dart
+++ lib/screens/settings/settings_home.dart
@@ -24,6 +24,7 @@ class _SettingsHomeScreenState extends State<SettingsHomeScreen> {
 
   static const EzSpacer spacer = EzSpacer();
   static const EzSeparator separator = EzSeparator();
+  static const EzDivider divider = EzDivider();
 
   late final ButtonStyle menuButtonStyle = TextButton.styleFrom(
     padding: EzInsets.wrap(EzConfig.get(paddingKey)),
@@ -49,53 +50,35 @@ class _SettingsHomeScreenState extends State<SettingsHomeScreen> {
   // Home list
   final List<String> homePackages = EzConfig.get(homePackagesKey) ??
       defaultConfig[homePackagesKey] as List<String>;
-
   late final List<String> appNames =
       provider.apps.map((AppInfo app) => app.label).toList();
-  late final List<DropdownMenuEntry<AppInfo>> dropdownPackages;
 
-  late AppInfo leftPackage;
-  late AppInfo rightPackage;
+  late final List<DropdownMenuEntry<AppInfo>> dropdownPackages =
+      <AppInfo>[nullApp, ...provider.apps]
+          .map((AppInfo app) => DropdownMenuEntry<AppInfo>(
+                value: app,
+                label: app.label,
+                style: menuButtonStyle,
+              ))
+          .toList();
+
+  final String storedLeft = EzConfig.get(leftPackageKey) ?? '';
+  final String storedRight = EzConfig.get(rightPackageKey) ?? '';
+
+  late AppInfo leftPackage = dropdownPackages
+      .firstWhere((DropdownMenuEntry<AppInfo> entry) =>
+          entry.value.package == storedLeft)
+      .value; // TODO: faster
+  late AppInfo rightPackage = dropdownPackages
+      .firstWhere((DropdownMenuEntry<AppInfo> entry) =>
+          entry.value.package == storedRight)
+      .value; // TODO: faster
 
   // Full list
   bool autoSearch = EzConfig.get(autoSearchKey) ?? defaultConfig[autoSearchKey];
   List<String>? hiddenPackages = EzConfig.get(hiddenPackagesKey);
   List<String>? nonZenPackages = EzConfig.get(nonZenPackagesKey);
   bool zenStream = EzConfig.get(zenStreamKey) ?? defaultConfig[zenStreamKey];
-
-  // Init //
-
-  @override
-  void initState() {
-    super.initState();
-
-    dropdownPackages = provider.apps
-        .map((AppInfo app) => DropdownMenuEntry<AppInfo>(
-              value: app,
-              label: app.label,
-              style: menuButtonStyle,
-            ))
-        .toList();
-    dropdownPackages.insert(
-        0,
-        DropdownMenuEntry<AppInfo>(
-          value: nullApp,
-          label: nullApp.label,
-          style: menuButtonStyle,
-        ));
-
-    final String? storedLeft = EzConfig.get(leftPackageKey) ?? '';
-    final String? storedRight = EzConfig.get(rightPackageKey) ?? '';
-
-    leftPackage = dropdownPackages
-        .firstWhere((DropdownMenuEntry<AppInfo> entry) =>
-            entry.value.package == storedLeft)
-        .value; // TODO: faster
-    rightPackage = dropdownPackages
-        .firstWhere((DropdownMenuEntry<AppInfo> entry) =>
-            entry.value.package == storedRight)
-        .value; // TODO: faster
-  }
 
   //* Return the build *//
   // TODO: Should some of these go into custom pre-existing screens?
@@ -155,7 +138,7 @@ class _SettingsHomeScreenState extends State<SettingsHomeScreen> {
               spacer,
 
               // tmp
-              const EzSwitchPair(text: 'WeatherPos2Layout', value: false),
+              const EzText('WeatherPos2Layout'),
               spacer,
               EzSwitchPair(
                 text: 'Hide status bar',
@@ -167,16 +150,16 @@ class _SettingsHomeScreenState extends State<SettingsHomeScreen> {
                   setState(() => hideStatusBar = value);
                 },
               ),
-              separator,
+              divider,
 
               // Home list //
 
               // Home list
-              const EzSwitchPair(text: 'Home packages', value: true),
+              const EzText('HomePackages2Home'),
               spacer,
 
               // tmp
-              const EzSwitchPair(text: 'HomeAlign2Layout', value: false),
+              const EzText('HomeAlign2Layout'),
               spacer,
 
               // Left swipe
@@ -219,16 +202,16 @@ class _SettingsHomeScreenState extends State<SettingsHomeScreen> {
                   )
                 ],
               ),
-              separator,
+              divider,
 
               // Full list //
 
               // tmp
-              const EzSwitchPair(text: 'FLAlign2Layout', value: false),
+              const EzText('FLAlign2Layout'),
               spacer,
 
               // tmp
-              const EzSwitchPair(text: 'ETile2Design', value: false),
+              const EzText('ETile2Design'),
               spacer,
 
               // Auto search
