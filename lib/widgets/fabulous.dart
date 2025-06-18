abfb797dbd6b8fb89c3701a2771e0c567a0b5eb6
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-18 11:13:39 -0700

    external onpressed

--- lib/widgets/fabulous.dart
+++ lib/widgets/fabulous.dart
@@ -6,17 +6,27 @@
 import '../screens/export.dart';
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
+class AddFAB extends FloatingActionButton {
+  /// [FloatingActionButton] that will open a modal for adding more home apps
+  AddFAB(BuildContext context, void Function()? onPressed, {super.key})
+      : super(
+          heroTag: 'add_fab',
+          onPressed: onPressed,
+          tooltip: 'Add another home app',
+          child: EzIcon(PlatformIcons(context).add),
+        );
+}
+
 class SettingsFAB extends FloatingActionButton {
-  /// [FloatingActionButton] that goes to the [SettingsHomeScreen]
-  SettingsFAB(BuildContext context, {super.key})
+  /// [FloatingActionButton] that will go to the [SettingsHomeScreen]
+  SettingsFAB(BuildContext context, void Function()? onPressed, {super.key})
       : super(
           heroTag: 'settings_fab',
-          child: EzIcon(PlatformIcons(context).settings),
-          onPressed: () => context.goNamed(settingsHomePath),
+          onPressed: onPressed,
           tooltip: ezL10n(context).ssNavHint,
+          child: EzIcon(PlatformIcons(context).settings),
         );
 }
