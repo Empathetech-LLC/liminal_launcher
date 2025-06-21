b2d294ad3aff47f9318a804f356e8b074025060f
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-20 17:38:16 -0700

    added map

--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -9,8 +9,15 @@ import 'package:flutter/material.dart';
 
 class AppInfoProvider extends ChangeNotifier {
   final List<AppInfo> _apps;
+  final Map<String, AppInfo> _appMap;
 
-  AppInfoProvider(List<AppInfo> apps) : _apps = apps;
+  AppInfoProvider(List<AppInfo> apps)
+      : _apps = apps,
+        _appMap = <String, AppInfo>{
+          for (AppInfo app in apps) app.package: app,
+        };
 
   List<AppInfo> get apps => _apps;
+
+  AppInfo? getAppFromID(String package) => _appMap[package];
 }
