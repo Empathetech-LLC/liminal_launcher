fc796b99a93d43a1ac6ee8af694c95903d0be506
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-22 22:42:10 -0700

    fragment activity

--- android/app/src/main/kotlin/net/empathetech/empathetech_launcher/MainActivity.kt
+++ android/app/src/main/kotlin/net/empathetech/empathetech_launcher/MainActivity.kt
@@ -12,13 +12,13 @@ import android.provider.Settings
 
 import androidx.annotation.NonNull
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 import java.io.ByteArrayOutputStream
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
   private val CHANNEL = "net.empathetech.liminal/query"
 
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
