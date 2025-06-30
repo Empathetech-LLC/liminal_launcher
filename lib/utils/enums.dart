76b446b18e941dc7583f1112e8a006ec5c70d315
empathictech <105748722+empathictech@users.noreply.github.com>
2025-06-29 17:27:02 -0700

    using local consts

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -3,25 +3,29 @@
  * See LICENSE for distribution and usage details.
  */
 
+import './export.dart';
 import 'package:flutter/material.dart';
 
 // BTS settings //
 
 enum ListSort { name, publisher }
 
+const String _name = 'name';
+const String _publisher = 'publisher';
+
 extension AppListSortConfig on ListSort {
   String get configValue {
     switch (this) {
       case ListSort.name:
-        return 'name';
+        return _name;
       case ListSort.publisher:
-        return 'publisher';
+        return _publisher;
     }
   }
 
   static ListSort fromValue(String value) {
     switch (value) {
-      case 'publisher':
+      case _publisher:
         return ListSort.publisher;
       default:
         return ListSort.name;
@@ -33,19 +37,22 @@ extension AppListSortConfig on ListSort {
 
 enum HeaderOrder { timeFirst, weatherFirst }
 
+const String _timeFirst = 'time_first';
+const String _weatherFirst = 'weather_first';
+
 extension HeaderOrderConfig on HeaderOrder {
   String get configValue {
     switch (this) {
       case HeaderOrder.timeFirst:
-        return 'time_first';
+        return _timeFirst;
       case HeaderOrder.weatherFirst:
-        return 'weather_first';
+        return _weatherFirst;
     }
   }
 
   static HeaderOrder fromValue(String value) {
     switch (value) {
-      case 'weather_first':
+      case _weatherFirst:
         return HeaderOrder.weatherFirst;
       default:
         return HeaderOrder.timeFirst;
@@ -55,15 +62,19 @@ extension HeaderOrderConfig on HeaderOrder {
 
 enum ListAlignment { center, start, end }
 
+const String _center = 'center';
+const String _start = 'start';
+const String _end = 'end';
+
 extension ListAlignmentConfig on ListAlignment {
   String get configValue {
     switch (this) {
       case ListAlignment.center:
-        return 'center';
+        return _center;
       case ListAlignment.start:
-        return 'start';
+        return _start;
       case ListAlignment.end:
-        return 'end';
+        return _end;
     }
   }
 
@@ -102,9 +113,9 @@ extension ListAlignmentConfig on ListAlignment {
 
   static ListAlignment fromValue(String value) {
     switch (value) {
-      case 'start':
+      case _start:
         return ListAlignment.start;
-      case 'end':
+      case _end:
         return ListAlignment.end;
       default:
         return ListAlignment.center;
@@ -116,27 +127,31 @@ extension ListAlignmentConfig on ListAlignment {
 
 enum LabelType { none, initials, full, wingding }
 
+const String _none = 'none';
+const String _initials = 'initials';
+const String _full = 'full';
+
 extension LabelTypeConfig on LabelType {
   String get configValue {
     switch (this) {
       case LabelType.none:
-        return 'none';
+        return _none;
       case LabelType.initials:
-        return 'initials';
+        return _initials;
       case LabelType.full:
-        return 'full';
+        return _full;
       case LabelType.wingding:
-        return 'wingding';
+        return wingding;
     }
   }
 
   static LabelType fromValue(String value) {
     switch (value) {
-      case 'none':
+      case _none:
         return LabelType.none;
-      case 'initials':
+      case _initials:
         return LabelType.initials;
-      case 'wingding':
+      case wingding:
         return LabelType.wingding;
       default:
         return LabelType.full;
