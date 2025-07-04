ba15b46152bc759d2fc02fd510e12f5e43a4753d
empathictech <105748722+empathictech@users.noreply.github.com>
2025-07-03 23:54:03 -0700

    using list view builder

--- lib/screens/app_list.dart
+++ lib/screens/app_list.dart
@@ -48,6 +48,8 @@ class _AppListScreenState extends State<AppListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final List<AppInfo> appList = provider.appList;
+
     return LiminalScaffold(
       GestureDetector(
         behavior: HitTestBehavior.opaque,
@@ -60,104 +62,104 @@ class _AppListScreenState extends State<AppListScreen> {
           }
         },
         child: LiminalScreen(
-          child: NotificationListener<ScrollNotification>(
-            onNotification: (ScrollNotification notification) {
-              if (notification is OverscrollNotification &&
-                  notification.overscroll < 0) {
-                // Pop on overscroll (when already the top)
-                if (atTop) {
-                  Navigator.of(context).pop();
-                  return true;
-                } else {
-                  setState(() => atTop = true);
-                  return true;
-                }
-              } else if (notification is ScrollUpdateNotification) {
-                if (atTop && notification.metrics.pixels > 0) {
-                  setState(() => atTop = false);
-                }
-              } else if (notification is ScrollEndNotification) {
-                setState(() =>
-                    atTop = (notification.metrics.pixels == 0) ? true : false);
-              }
-              return false; // Let other notifications propagate
-            },
-            child: EzScrollView(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: listAlign.crossAxis,
-              physics: const ClampingScrollPhysics(),
-              children: <Widget>[
-                if (spacing > margin) EzSpacer(space: spacing - margin),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: listAlign.crossAxis,
+            children: <Widget>[
+              if (spacing > margin) EzSpacer(space: spacing - margin),
 
-                // List controls
-                EzScrollView(
-                  scrollDirection: Axis.horizontal,
-                  mainAxisAlignment: listAlign.mainAxis,
-                  children: <Widget>[
-                    // Sort
-                    EzDropdownMenu<ListSort>(
-                      widthEntries: <String>['Publisher'],
-                      dropdownMenuEntries: const <DropdownMenuEntry<ListSort>>[
-                        DropdownMenuEntry<ListSort>(
-                          value: ListSort.name,
-                          label: 'Name',
-                        ),
-                        DropdownMenuEntry<ListSort>(
-                          value: ListSort.publisher,
-                          label: 'Publisher',
-                        ),
-                      ],
-                      enableSearch: false,
-                      initialSelection: listSort,
-                      onSelected: (ListSort? choice) async {
-                        if (choice == null) return;
-                        listSort = choice;
+              // List controls
+              EzScrollView(
+                scrollDirection: Axis.horizontal,
+                mainAxisAlignment: listAlign.mainAxis,
+                children: <Widget>[
+                  // Sort
+                  EzDropdownMenu<ListSort>(
+                    widthEntries: <String>['Publisher'],
+                    dropdownMenuEntries: const <DropdownMenuEntry<ListSort>>[
+                      DropdownMenuEntry<ListSort>(
+                        value: ListSort.name,
+                        label: 'Name',
+                      ),
+                      DropdownMenuEntry<ListSort>(
+                        value: ListSort.publisher,
+                        label: 'Publisher',
+                      ),
+                    ],
+                    enableSearch: false,
+                    initialSelection: listSort,
+                    onSelected: (ListSort? choice) async {
+                      if (choice == null) return;
+                      listSort = choice;
 
-                        await EzConfig.setString(
-                          appListSortKey,
-                          listSort.configValue,
-                        );
-                        provider.sort(listSort, ascList);
+                      await EzConfig.setString(
+                        appListSortKey,
+                        listSort.configValue,
+                      );
+                      provider.sort(listSort, ascList);
 
-                        setState(() {});
-                      },
+                      setState(() {});
+                    },
+                  ),
+                  const EzSpacer(vertical: false),
+
+                  // Order
+                  EzIconButton(
+                    icon: EzIcon(
+                        ascList ? Icons.arrow_upward : Icons.arrow_downward),
+                    onPressed: () async {
+                      ascList = !ascList;
+
+                      await EzConfig.setBool(appListOrderKey, ascList);
+                      provider.sort(listSort, ascList);
+
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
+              spacer,
+
+              // App list
+              NotificationListener<ScrollNotification>(
+                onNotification: (ScrollNotification notification) {
+                  if (notification is OverscrollNotification &&
+                      notification.overscroll < 0) {
+                    // Pop on overscroll (when already the top)
+                    if (atTop) {
+                      Navigator.of(context).pop();
+                      return true;
+                    } else {
+                      setState(() => atTop = true);
+                      return true;
+                    }
+                  } else if (notification is ScrollUpdateNotification) {
+                    if (atTop && notification.metrics.pixels > 0) {
+                      setState(() => atTop = false);
+                    }
+                  } else if (notification is ScrollEndNotification) {
+                    setState(() => atTop =
+                        (notification.metrics.pixels == 0) ? true : false);
+                  }
+                  return false;
+                },
+                child: ListView.builder(
+                  physics: const ClampingScrollPhysics(),
+                  itemCount: appList.length,
+                  itemBuilder: (_, int index) => Padding(
+                    padding: EdgeInsets.symmetric(vertical: spacing / 2),
+                    key: ValueKey<String>(appList[index].keyLabel),
+                    child: AppTile(
+                      app: appList[index],
+                      onHomeScreen: false,
+                      editing: false,
+                      refreshHome: widget.refreshHome,
                     ),
-                    const EzSpacer(vertical: false),
-
-                    // Order
-                    EzIconButton(
-                      icon: EzIcon(
-                          ascList ? Icons.arrow_upward : Icons.arrow_downward),
-                      onPressed: () async {
-                        ascList = !ascList;
-
-                        await EzConfig.setBool(appListOrderKey, ascList);
-                        provider.sort(listSort, ascList);
-
-                        setState(() {});
-                      },
-                    ),
-                  ],
+                  ),
                 ),
-                spacer,
-
-                // App list
-                ...provider.apps
-                    .where((AppInfo app) =>
-                        !provider.hiddenPS.contains(app.package))
-                    .expand((AppInfo app) => <Widget>[
-                          AppTile(
-                            key: ValueKey<String>(app.keyLabel),
-                            app: app,
-                            onHomeScreen: false,
-                            editing: false,
-                            refreshHome: widget.refreshHome,
-                          ),
-                          spacer,
-                        ]),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
