0d990cf3b7d0c3cc6044c883dc65dfd9b6d76d3c
empathictech <105748722+empathictech@users.noreply.github.com>
2025-07-08 16:42:21 -0700

    search rough draft

--- lib/screens/app_list.dart
+++ lib/screens/app_list.dart
@@ -24,9 +24,13 @@ class _AppListScreenState extends State<AppListScreen> {
   // Gather the theme data //
 
   static const EzSpacer spacer = EzSpacer();
+  static const EzSpacer rowSpacer = EzSpacer(vertical: false);
 
   final double margin = EzConfig.get(marginKey);
   final double spacing = EzConfig.get(spacingKey);
+
+  late final EdgeInsets listPadding =
+      EdgeInsets.symmetric(vertical: spacing / 2);
 
   // Define the build data //
 
@@ -42,27 +46,36 @@ class _AppListScreenState extends State<AppListScreen> {
   bool ascList =
       EzConfig.get(appListOrderKey) ?? EzConfig.getDefault(appListOrderKey);
 
+  final ScrollController scrollControl = ScrollController();
+
+  late final List<AppInfo> appList = provider.apps
+      .where((AppInfo app) => !provider.hiddenSet.contains(app.id))
+      .toList();
+
+  late List<AppInfo> searchList = appList;
+  final TextEditingController searchControl = TextEditingController();
+  bool searching =
+      EzConfig.get(autoSearchKey) ?? EzConfig.getDefault(autoSearchKey);
+
   bool atTop = true;
   bool atBottom = false;
 
-  final ScrollController appScrollControl = ScrollController();
-  final ScrollController searchScrollControl = ScrollController();
+  // Define custom functions //
 
-  final TextEditingController searchController = TextEditingController();
-  final bool autoSearch =
-      EzConfig.get(autoSearchKey) ?? EzConfig.getDefault(autoSearchKey);
+  void refreshAll() {
+    widget.refreshHome?.call();
+    setState(() {});
+  }
+
+  List<AppInfo> searchApps(List<AppInfo> appList) => appList
+      .where((AppInfo app) =>
+          app.name.toLowerCase().contains(searchControl.text.toLowerCase()))
+      .toList();
 
   // Return the build //
 
   @override
   Widget build(BuildContext context) {
-    final List<AppInfo> appList = provider.appList;
-    final List<AppInfo> searchList = provider.appList
-        .where((AppInfo app) => app.name
-            .toLowerCase()
-            .contains(searchController.text.toLowerCase()))
-        .toList();
-
     return LiminalScaffold(
       GestureDetector(
         behavior: HitTestBehavior.opaque,
@@ -77,7 +90,6 @@ class _AppListScreenState extends State<AppListScreen> {
         child: EzScreen(
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: listAlign.crossAxis,
             children: <Widget>[
               if (spacing > margin) EzSpacer(space: spacing - margin),
@@ -96,6 +108,7 @@ class _AppListScreenState extends State<AppListScreen> {
                       icon: const Icon(Icons.sort),
                     ),
                     menuChildren: <EzMenuButton>[
+                      // By name
                       EzMenuButton(
                         label: 'Name',
                         textAlign: listAlign.textAlign,
@@ -111,6 +124,7 @@ class _AppListScreenState extends State<AppListScreen> {
                           setState(() {});
                         },
                       ),
+                      // By publisher
                       EzMenuButton(
                         label: 'Publisher',
                         textAlign: listAlign.textAlign,
@@ -128,7 +142,7 @@ class _AppListScreenState extends State<AppListScreen> {
                       ),
                     ],
                   ),
-                  const EzSpacer(vertical: false),
+                  rowSpacer,
 
                   // Order
                   EzIconButton(
@@ -145,6 +159,44 @@ class _AppListScreenState extends State<AppListScreen> {
                   ),
 
                   // Search
+                  AnimatedContainer(
+                    duration: animDuration,
+                    width: searching ? 200 : null,
+                    curve: Curves.easeInOut,
+                    child: EzRow(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        EzIconButton(
+                          icon: Icon(PlatformIcons(context).search),
+                          onPressed: () {
+                            if (searching) {
+                              closeKeyboard(context);
+                              searchControl.clear();
+                              searchList = appList;
+                              setState(() => searching = false);
+                            } else {
+                              searchList = searchApps(appList);
+                              setState(() => searching = true);
+                            }
+                          },
+                        ),
+                        if (searching)
+                          Expanded(
+                            child: TextField(
+                              controller: searchControl,
+                              autofocus: searching,
+                              decoration: const InputDecoration(
+                                hintText: 'Search',
+                                border: InputBorder.none,
+                                isDense: true,
+                              ),
+                              onChanged: (_) => setState(
+                                  () => searchList = searchApps(appList)),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
               spacer,
@@ -180,41 +232,43 @@ class _AppListScreenState extends State<AppListScreen> {
                   }
                   return false;
                 },
-                child: Expanded(
-                  child: searchController.text.isNotEmpty
-                      ? ListView.builder(
-                          controller: searchScrollControl,
+                child: searching
+                    ? Expanded(
+                        key: const ValueKey<String>('searchList'),
+                        child: ListView.builder(
+                          controller: scrollControl,
                           physics: const ClampingScrollPhysics(),
                           itemCount: searchList.length,
                           itemBuilder: (_, int index) => Padding(
-                            padding:
-                                EdgeInsets.symmetric(vertical: spacing / 2),
                             key: ValueKey<String>(searchList[index].id),
+                            padding: listPadding,
                             child: AppTile(
                               app: searchList[index],
                               onHomeScreen: false,
                               editing: false,
-                              refreshHome: widget.refreshHome,
+                              refresh: refreshAll,
                             ),
                           ),
-                        )
-                      : ListView.builder(
-                          controller: appScrollControl,
+                        ),
+                      )
+                    : Expanded(
+                        key: const ValueKey<String>('appList'),
+                        child: ListView.builder(
+                          controller: scrollControl,
                           physics: const ClampingScrollPhysics(),
                           itemCount: appList.length,
                           itemBuilder: (_, int index) => Padding(
-                            padding:
-                                EdgeInsets.symmetric(vertical: spacing / 2),
+                            padding: listPadding,
                             key: ValueKey<String>(appList[index].id),
                             child: AppTile(
                               app: appList[index],
                               onHomeScreen: false,
                               editing: false,
-                              refreshHome: widget.refreshHome,
+                              refresh: refreshAll,
                             ),
                           ),
                         ),
-                ),
+                      ),
               ),
             ],
           ),
@@ -227,9 +281,9 @@ class _AppListScreenState extends State<AppListScreen> {
           if (!atTop)
             FloatingActionButton(
               onPressed: () {
-                appScrollControl.animateTo(
+                scrollControl.animateTo(
                   0,
-                  duration: const Duration(milliseconds: 250),
+                  duration: animDuration,
                   curve: Curves.easeOut,
                 );
               },
@@ -243,9 +297,9 @@ class _AppListScreenState extends State<AppListScreen> {
           if (!atBottom)
             FloatingActionButton(
               onPressed: () {
-                appScrollControl.animateTo(
-                  appScrollControl.position.maxScrollExtent,
-                  duration: const Duration(milliseconds: 250),
+                scrollControl.animateTo(
+                  scrollControl.position.maxScrollExtent,
+                  duration: animDuration,
                   curve: Curves.easeOut,
                 );
               },
@@ -258,8 +312,8 @@ class _AppListScreenState extends State<AppListScreen> {
 
   @override
   void dispose() {
-    appScrollControl.dispose();
-    searchController.dispose();
+    scrollControl.dispose();
+    searchControl.dispose();
     super.dispose();
   }
 }
