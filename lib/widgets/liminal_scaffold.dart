2f138148b6b26d6b27fae8a4d236eea998b2be35
empathictech <105748722+empathictech@users.noreply.github.com>
2025-07-09 13:05:39 -0700

    testing new screen... wallpaper is just behind the app it seems

--- lib/widgets/liminal_scaffold.dart
+++ lib/widgets/liminal_scaffold.dart
@@ -3,6 +3,8 @@
  * See LICENSE for distribution and usage details.
  */
 
+import '../utils/export.dart';
+
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
@@ -19,7 +21,7 @@ class LiminalScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) => EzAdaptiveScaffold(
         small: Scaffold(
-          body: SafeArea(child: body),
+          body: body,
           floatingActionButton: fab,
           floatingActionButtonLocation: EzConfig.get(isLeftyKey) ?? false
               ? FloatingActionButtonLocation.startFloat
@@ -27,4 +29,65 @@ class LiminalScaffold extends StatelessWidget {
           resizeToAvoidBottomInset: false,
         ),
       );
+}
+
+class LiminalScreen extends StatelessWidget {
+  /// [Container.alignment] passthrough
+  final AlignmentGeometry? alignment;
+
+  /// Screen width
+  final double width;
+
+  /// Screen height
+  final double height;
+
+  /// [Container.clipBehavior] passthrough
+  final Clip clipBehavior;
+
+  /// Screen content
+  final Widget child;
+
+  /// Modified [EzScreen]
+  const LiminalScreen({
+    super.key,
+    this.alignment,
+    this.width = double.infinity,
+    this.height = double.infinity,
+    this.clipBehavior = Clip.none,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final EdgeInsetsGeometry screenMargin =
+        EdgeInsets.all(EzConfig.get(marginKey));
+
+    Decoration? buildDecoration() {
+      final String decorationKey = isDarkTheme(context)
+          ? darkBackgroundImageKey
+          : lightBackgroundImageKey;
+      final String? imagePath = EzConfig.get(decorationKey);
+
+      if (imagePath == null || imagePath == noImageValue) {
+        return null;
+      } else {
+        final BoxFit? fit =
+            ezFitFromName(EzConfig.get('$decorationKey$boxFitSuffix'));
+
+        return BoxDecoration(
+          image: DecorationImage(image: ezImageProvider(imagePath), fit: fit),
+        );
+      }
+    }
+
+    return Container(
+      alignment: alignment,
+      padding: screenMargin,
+      decoration: (EzConfig.get(useOSKey) == false) ? buildDecoration() : null,
+      width: width,
+      height: height,
+      clipBehavior: clipBehavior,
+      child: SafeArea(child: child),
+    );
+  }
 }
